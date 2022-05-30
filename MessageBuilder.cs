e23b43adbf86ccde08f0e2564e910ba5f541d137
Chris Preston <Chris_Preston@waters.com>
2022-05-30 14:37:03 +0100

    File scope namespaces

--- MessageBuilder.cs
+++ MessageBuilder.cs
@@ -1,22 +1,21 @@
 using System;
 using System.Globalization;
 
-namespace jogobom.MadeSandwich
+namespace jogobom.MadeSandwich;
+
+public static class MessageBuilder
 {
-    public static class MessageBuilder
+    private static readonly string[] PossibleIntros = {"Sotd.", "Sotd:", "Sotd", "SOTD", "SOTD:", "SOTD."};
+
+    public static string Build(Sandwich sandwich)
     {
-        private static readonly string[] PossibleIntros = {"Sotd.", "Sotd:", "Sotd", "SOTD", "SOTD:", "SOTD."};
+        var specificCulture = CultureInfo.CreateSpecificCulture("en-GB");
 
-        public static string Build(Sandwich sandwich)
-        {
-            var specificCulture = CultureInfo.CreateSpecificCulture("en-GB");
+        return $"{GetRandomIntro()} {sandwich.ContentDescription} {sandwich.BreadAdvice} {sandwich.Price.ToString("C", specificCulture)}";
+    }
 
-            return $"{GetRandomIntro()} {sandwich.ContentDescription} {sandwich.BreadAdvice} {sandwich.Price.ToString("C", specificCulture)}";
-        }
-
-        private static string GetRandomIntro()
-        {
-            return PossibleIntros[new Random().Next(PossibleIntros.Length)];
-        }
+    private static string GetRandomIntro()
+    {
+        return PossibleIntros[new Random().Next(PossibleIntros.Length)];
     }
 }
--- SandwichTime.cs
+++ SandwichTime.cs
@@ -3,23 +3,22 @@ using System.Threading.Tasks;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 
-namespace jogobom.MadeSandwich
+namespace jogobom.MadeSandwich;
+
+public class SandwichTime
 {
-    public class SandwichTime
+    [FunctionName("SandwichTime")]
+    public async Task RunAsync([TimerTrigger("0 30 9 * * 1-5")]TimerInfo myTimer, ILogger log)
     {
-        [FunctionName("SandwichTime")]
-        public async Task RunAsync([TimerTrigger("0 30 9 * * 1-5")]TimerInfo myTimer, ILogger log)
-        {
-            log.LogInformation($"SandwichTime trigger function executed at: {DateTime.Now}");
+        log.LogInformation($"SandwichTime trigger function executed at: {DateTime.Now}");
 
-            var generator = new SandwichGenerator();
+        var generator = new SandwichGenerator();
 
-            var sandwich = generator.Generate();
+        var sandwich = generator.Generate();
 
-            var message = MessageBuilder.Build(sandwich);
-            log.LogInformation(message);
+        var message = MessageBuilder.Build(sandwich);
+        log.LogInformation(message);
 
-            await Slack.PostToFood(message);
-        }
+        await Slack.PostToFood(message);
     }
 }
